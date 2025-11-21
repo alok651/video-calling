8d51e4773a0b42f5eda13fe74d49ec265563a25a
alok651 <alokkr651@gmail.com>
2025-11-21 20:53:19 +0530

    abs

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,43 +1,41 @@
-import express from 'express';
-import dotenv from 'dotenv';
+import express from 'express'
 import {ENV} from './config/env.js';
 import { connectDB } from './config/db.js';
-import {clerkMiddleware} from "@clerk/express";
-import { inngest, functions } from './config/inngest.js';
-import { serve } from "inngest/express";
-
-dotenv.config();
-
-const app=express();
+import { clerkMiddleware } from '@clerk/express';
+// import { clerkClient } from '@clerk/express';
+import { inngest , functions } from './config/inngest.js';
+import { serve } from 'inngest/express';
 
 
-app.use(clerkMiddleware())//req.auth will be available
+const app = express();
+app.use(express.json()); //middleware to parse json body
 
-app.use(express.json());//req.body will be available
-app.use("/api/inngest", serve({ client: inngest, functions }));
 
-app.get('/',(req,res)=>{
-    res.send('Hello, World!');
+app.use(clerkMiddleware()); //req.auth property will be available
+
+app.use(clerkMiddleware({ secretKey: ENV.CLERK_SECRET_KEY }));
+
+app.use("/api/inngest", serve ({ client : inngest , functions}));   
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
 });
 
 
-const startServer=async()=>{
-    try{
-        await connectDB();
-        if(ENV.NODE_ENV!=="production"){
-            app.listen(ENV.PORT,()=>{
-                console.log("Server started on port ",ENV.PORT);
-            });
-        }
 
-    }catch(error){
-        console.error("Error starting server: ",error);
-        process.exit(1);
-    }
-} ;
+const startServer = async () => {
+    try {
+        await connectDB();
+        if (!ENV.NODE_ENV !== 'production') {
+            app.listen(ENV.PORT, () => {
+                console.log("Server is running on port:" , ENV.PORT);
+            });
+        }} catch (error) {
+        console.error("Error starting server:", error);
+        process.exit(1); //1 means failure 0 means success
+    }};
+
+
 startServer();
 
-export default app;
-
-
-
+export default app;
