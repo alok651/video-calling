cd53a296aff01a5f68d3544a0f815eec1e577104
alok651 <alokkr651@gmail.com>
2025-11-25 19:08:54 +0530

    provider

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,27 +1,58 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-// import './index.css'
-import App from './App.jsx'
-import { ClerkProvider } from '@clerk/clerk-react';
-import { BrowserRouter } from 'react-router';
+import React, { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { ClerkProvider } from "@clerk/clerk-react";
 
+import {
+  BrowserRouter,
+  useLocation,
+  useNavigationType,
+  createRoutesFromChildren,
+  matchRoutes,
+} from "react-router";
 
-// Import your Publishable Key
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+import { Toaster } from "react-hot-toast";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import AuthProvider from "./providers/AuthProvider.jsx";
+
+import * as Sentry from "@sentry/react";
+
+const queryClient = new QueryClient();
+
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
 if (!PUBLISHABLE_KEY) {
-  throw new Error('Missing Publishable Key')
+  throw new Error("Missing Publishable Key");
 }
 
+Sentry.init({
+  dsn: "https://969f6036102345412dd11367845cbc1a@o4510397491314688.ingest.us.sentry.io/4510419932807168",
+  integrations: [
+    Sentry.reactRouterV7BrowserTracingIntegration({
+      useEffect: React.useEffect,
+      useLocation,
+      useNavigationType,
+      createRoutesFromChildren,
+      matchRoutes,
+    }),
+  ],
+  tracesSampleRate: 1.0, // <-- Missing comma was here
+});
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-        <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
-          <BrowserRouter>
-         
-              <App />
-            </BrowserRouter>  
-        </ClerkProvider>
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+      <BrowserRouter>
+        <QueryClientProvider client={queryClient}>
 
-  </StrictMode>,
-)
+          <AuthProvider>
+            <App />
+          </AuthProvider>
+
+          <Toaster position="top-right" />
+        </QueryClientProvider>
+      </BrowserRouter>
+    </ClerkProvider>
+  </StrictMode>
+);
