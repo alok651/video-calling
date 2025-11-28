bf0e227f6adae6d866ec2aed1e93d081169b08e4
alok651 <alokkr651@gmail.com>
2025-11-28 18:43:42 +0530

    callpage

--- frontend/src/pages/Callpage.jsx
+++ frontend/src/pages/Callpage.jsx
@@ -1,11 +1,11 @@
- import { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router";
 import { useQuery } from "@tanstack/react-query";
 import { useUser } from "@clerk/clerk-react";
 import toast from "react-hot-toast";
 
 import { getStreamToken } from "../../lib/api";
-
+ 
 import {
   StreamVideo,
   StreamVideoClient,
