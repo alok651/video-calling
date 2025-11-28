a8f008a71f6271a2552af594feef04564475271c
alok651 <alokkr651@gmail.com>
2025-11-28 20:42:22 +0530

    final

--- frontend/src/pages/Callpage.jsx
+++ frontend/src/pages/Callpage.jsx
@@ -5,7 +5,7 @@ import { useUser } from "@clerk/clerk-react";
 import toast from "react-hot-toast";
 
 import { getStreamToken } from "../../lib/api";
- 
+
 import {
   StreamVideo,
   StreamVideoClient,
